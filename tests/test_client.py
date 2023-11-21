3242c75f034c29c1df98b5f9fbea80e6b8ced556
Dmitry Trifonov <slonegg@gmail.com>
2023-11-20 17:06:36 -0800

    add assertion to test process output

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,10 +3,14 @@ def test_authentication(fair_client):
     pass
 
 
-def test_echo_job(fair_client):
+def test_echo_job(fair_client, capsys):
     assert fair_client.run(image='alpine', command=['echo', 'hello fair compute']) == 0
+    captured = capsys.readouterr()
+    assert captured.out == "hello fair compute"
 
 
-def test_echo_program(fair_client):
+def test_echo_program(fair_client, capsys):
     nodes = fair_client.get_nodes()
     assert fair_client.run(image='alpine', node=nodes[0]['node_id'], command=['echo', 'hello fair compute']) == 0
+    captured = capsys.readouterr()
+    assert captured.out == "hello fair compute"
